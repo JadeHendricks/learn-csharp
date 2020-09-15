23d7ed1f630fbc3e4bc19c929da1564897196d63
jadehendricks <hendricksjade@gmail.com>
2020-09-15 15:10:59 +0200

    get user input and build a simple calculator

---
+++ learn-csharp/BuildingACalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn_csharp
+{
+    class BuildingACalculator
+    {
+        static void Building_A_Calculator(string[] args)
+        {
+            Console.Write("Enter a numer: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());            
+            Console.Write("Enter another numer: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ learn-csharp/GettingUserInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn_csharp
+{
+    class GettingUserInput
+    {
+        static void Getting_User_Input(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age);
+            Console.ReadLine();
+        }
+    }
+}
