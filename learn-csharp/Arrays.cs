a9b11eaa58a60c062c3aec5c0935f180e2f33f41
jadehendricks <hendricksjade@gmail.com>
2020-09-15 15:45:59 +0200

    workng with arrays and methods:void and methods that return a value.

---
+++ learn-csharp/Arrays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn_csharp
+{
+    class Arrays
+    {
+        static void Working_With_Arrays(string[] args)
+        {
+            int[] luckyNumbers = {4, 8, 16, 20, 22, 7};
+            string[] friends = new string[5]; //the number tells C# how many numbers we want the array to hold
+
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+            // getting a single value from an array via index
+            Console.WriteLine(luckyNumbers[0]);
+
+            // changing a single value in an array via index
+            luckyNumbers[1] = 900;
+            Console.WriteLine(luckyNumbers[1]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ learn-csharp/Methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn_csharp
+{
+    class Methods
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Jade", 23);
+            Console.WriteLine(cube(5));
+
+            int cubedNumber = cube(7);
+            Console.WriteLine(cubedNumber);
+        }
+
+        static int cube (int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+
+        static void SayHi (string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age + " years old");
+        }
+    }
+}
