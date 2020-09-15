a6f83d32621a4db02fbd74e9ddcb5c1bff6f5b77
jadehendricks <hendricksjade@gmail.com>
2020-09-15 17:49:42 +0200

    using while loops

---
+++ learn-csharp/WhileLoops.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn_csharp
+{
+    class WhileLoops
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            int index2 = 6;
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index2 <= 5);
+        }
+    }
+}
