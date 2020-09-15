0eaef4e384ce5ccd3c5fc3b12935b2fe5b04582a
jadehendricks <hendricksjade@gmail.com>
2020-09-15 21:05:45 +0200

    working with static class/methods

---
+++ learn-csharp/StaticMethodClasses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn_csharp
+{
+    // now we are not able to create an instance of this class because of static
+    static class StaticMethodClasses
+    {
+        public static void sayHi (string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
---
+++ learn-csharp/StaticMethodClassesParent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn_csharp
+{
+    class StaticMethodClassesParent
+    {
+        static void Main_StaticMethodClassesParent(string[] args)
+        {
+            StaticMethodClasses.sayHi("Jade");
+        }
+    }
+}
